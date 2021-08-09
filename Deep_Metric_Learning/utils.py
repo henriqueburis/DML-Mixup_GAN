1c982ab3c9cf73a354919cd231aba0899036c1a0
Luiz Henrique Buris <henrique.buris@outlook.com.br>
2021-08-09 09:17:57 -0300

    Add files via upload
    file utils update with metric  hloss, topk, distances

--- Deep_Metric_Learning/utils.py
+++ Deep_Metric_Learning/utils.py
@@ -1,5 +1,9 @@
+import torch
+import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from sklearn.metrics import pairwise_distances
 
 def scatter(x, colors):
     # We choose a color palette with seaborn.
@@ -59,4 +63,70 @@ def chebyshev(features_u,features_l):
   for line in range(features_u.shape[0]):
     dist.append((torch.max(torch.abs(features_u[line]-features_l),dim=1).values).numpy())
   return np.array(dist)
+
+
+def MCScore(log_prob):
+  top2 = torch.topk(log_prob, k=2, dim=1).values[:,1]
+  top1 = torch.topk(log_prob, k=2, dim=1).values[:,0]
+  score_c = (top2 - top1) / torch.sum(log_prob,dim=1)
+  return score_c
+
+
+
+class HLoss(nn.Module):
+    def __init__(self):
+        super(HLoss, self).__init__()
+
+    def forward(self, x):
+        #print(x)
+        #b = F.softmax(x, dim=1) * F.log_softmax(x, dim=1)
+        b = x*np.log(x)
+        b = np.sum(b,axis=1)
+        #b = -1.0 * b.sum()
+        b = -1.0 * b
+        return b
+
+
+
+#HLoss = HLoss()
+
+
+def TopK(final_dist,label_l,true_labels, k):
+  final_dist = np.squeeze(np.array(final_dist))
+  k = 20
+  freq = np.zeros(100)
+  topk = np.sort(final_dist)[0:k] # select top k
+
+  for i in range(topk.shape[0]):
+     pos = np.where(topk[i]==final_dist)[0]
+     freq[label_l[pos]] += 1
+
+  final_index = np.argmax(freq)
+  return  final_index
+
+def pairwise_distances_(feature_u, img_u, label_u, feature_l, img_l, label_l, true_labels):
+  labels = []
+  correct = 0
+  erro = 0
+
+  dist_matrix_1 = pairwise_distances(np.array([feature_u]),feature_l , metric = 'euclidean')  #dist_matrix_1.shape(1,5000)
+  dist_matrix_scaler_1 = (dist_matrix_1 - dist_matrix_1.min()) / (dist_matrix_1.max() - dist_matrix_1.min())
+
+  dist_matrix_4 = pairwise_distances(np.array([feature_u]),feature_l , metric = 'chebyshev')
+  dist_matrix_scaler_4 = (dist_matrix_4 - dist_matrix_4.min()) / (dist_matrix_4.max() - dist_matrix_4.min())
+
+  dist_matrix_5 = pairwise_distances(np.array([feature_u]),feature_l , metric = 'cityblock')
+  dist_matrix_scaler_5 = (dist_matrix_5 - dist_matrix_5.min()) / (dist_matrix_5.max() - dist_matrix_5.min())
+
+  final_dist = (1+dist_matrix_scaler_1) * (1+dist_matrix_scaler_5) * (1+dist_matrix_scaler_4)
+  
+  k = args.topk
+  final_index = TopK(final_dist,label_l, true_labels,k)
+
+  if(true_labels == final_index): #label_l[final_index]):
+    correct = correct +1;
+  else:
+    erro = erro + 1
+
+  return correct,erro
 
